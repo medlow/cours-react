364db7c6daf1eeb11ab063c76cdd081568606270
medlow <medlow00@gmail.com>
2022-01-01 12:36:40 +0000

    v2

---
+++ src/pages/About.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+  return (
+    <div>
+      <Navigation/>
+      <h1>About</h1>
+      <br/>
+      <p>
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+        
+      </p>
+      <br/>
+      <p>
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+      Lorem ipsum dolor sit amet, consectet
+        
+      </p>
+    </div>
+  );
+};
+
+export default About;
